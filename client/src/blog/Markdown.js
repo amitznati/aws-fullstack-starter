baf976a431a261612f46561469f535dcc779adc3
amitznati-work-hp <amitznati@gmail.com>
2021-01-19 10:14:38 +0200

    Revert "initiat blog"
    This reverts commit eb91253c093c349894789611148c356ecdadb71a.

--- client/src/blog/Markdown.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactMarkdown from 'react-markdown-it';
-
-export default function Markdown(props) {
-  return <ReactMarkdown source={props.children} />
-}
--- client/src/index.js
+++ client/src/index.js
@@ -1,40 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
-import {CssBaseline, Container} from '@material-ui/core';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-import {BrowserRouter as Router} from "react-router-dom";
-import Header from './layout/Header';
-import Footer from './layout/Footer';
-import Routes from './layout/Routes';
-
 
 const client = new ApolloClient({
     uri: 'http://localhost:4000/graphql',
     cache: new InMemoryCache()
 });
-const sections = [
-    { title: 'Technology', url: '#' },
-    { title: 'Design', url: '#' },
-    { title: 'Culture', url: '#' },
-    { title: 'Business', url: '#' },
-    { title: 'Politics', url: '#' },
-    { title: 'Opinion', url: '#' },
-    { title: 'Science', url: '#' },
-    { title: 'Health', url: '#' },
-    { title: 'Style', url: '#' },
-    { title: 'Travel', url: '#' },
-];
+
 function App() {
     return (
         <ApolloProvider client={client}>
-            <Router>
-                <CssBaseline />
-                <Container maxWidth="lg">
-                    <Header title="Blog" sections={sections} />
-                    <Routes />
-                    <Footer title="Footer" description="Something here to give the footer a purpose!" />
-                </Container>
-            </Router>
+            <div>
+                <h2>My first Apollo app 🚀</h2>
+            </div>
         </ApolloProvider>
     );
 }
