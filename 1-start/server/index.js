52e453accba2b60fa57b169e690aaa51e8f58225
amitznati-work-hp <amitznati@gmail.com>
2021-01-19 17:37:16 +0200

    steps - 1-start

---
+++ 1-start/server/index.js
@@ -0,0 +1,45 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+    type Post {
+        title: String!
+        body: String!
+		id: ID!
+    }
+    type Query {
+        posts: [Post]
+    }
+    type Mutation {
+        createPost(title: String!, body: String!): Post
+    }
+`;
+const posts = [
+	{
+		title: 'The Awakening',
+		body: 'Kate Chopin',
+		id: '1111'
+	},
+	{
+		title: 'City of Glass',
+		body: 'Paul Auster',
+		id: '1112'
+	},
+];
+
+const resolvers = {
+	Query: {
+		posts: () => posts,
+	},
+	Mutation: {
+		createPost: (parent, args) => {
+			console.log(args);
+		}
+	}
+};
+
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+	console.log(`🚀  Server ready at ${url}`);
+});
