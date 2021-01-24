fc0db8d148dd2073ff9a6f7a16885ca105a9d5ef
amitznati-work-hp <amitznati@gmail.com>
2021-01-24 23:16:38 +0200

    update readme

--- 1-start/server/index.js
+++ 1-start/server/index.js
@@ -1,35 +1,35 @@
 const { ApolloServer, gql } = require('apollo-server');
 
 const typeDefs = gql`
-    type Book {
-        title: String
-        author: String
-    }
-    type Query {
-        books: [Book]
-    }
+  type Book {
+    title: String
+    author: String
+  }
+  type Query {
+    books: [Book]
+  }
 `;
 
 const resolvers = {
-	Query: {
-		books: () => books,
-	},
+  Query: {
+  books: () => books,
+  },
 };
 
 
 const books = [
-	{
-		title: 'The Awakening',
-		author: 'Kate Chopin',
-	},
-	{
-		title: 'City of Glass',
-		author: 'Paul Auster',
-	},
+  {
+  title: 'The Awakening',
+  author: 'Kate Chopin',
+  },
+  {
+  title: 'City of Glass',
+  author: 'Paul Auster',
+  },
 ];
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
-	console.log(`🚀  Server ready at ${url}`);
+  console.log(`🚀  Server ready at ${url}`);
 });
